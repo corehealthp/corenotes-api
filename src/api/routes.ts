a843b7d31a8e847cc1063547ecf43a02a0f28033
corehealthp <145934905+corehealthp@users.noreply.github.com>
2023-11-16 19:01:36 +0100

    Updated version number

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -28,7 +28,7 @@ export default function routes() {
         res.json({
             application: "Corenotes API",
             message: "Endpoint working",
-            version: "1.0.6"
+            version: "1.0.7"
         })
     });
 

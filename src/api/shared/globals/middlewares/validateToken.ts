d5a7f5a5f6f88707abe84b745d90781846979b56
Ogundipe Olusegun <emma1994204@gmail.com>
2023-12-14 02:14:33 -0800

    ValidateToken.ts code reverted

--- src/api/shared/globals/middlewares/validateToken.ts
+++ src/api/shared/globals/middlewares/validateToken.ts
@@ -1,73 +1,76 @@
-
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 import { sendFailureResponse } from "../server/serverResponse";
 import { NotAuthorizedError } from "../server/Error";
 import { getAuthUserByAuthAccessToken } from "../../services/db/auth.service";
 
-export default function validateToken(req:Request,res:Response,next:NextFunction){
-    let token:string | undefined;
+export default function validateToken (req:Request, res:Response, next:NextFunction) {
+    let token:string  = req.headers.authorization! ?? req.headers.cookie;
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('bearer')){
-        token = req.headers.authorization.split(' ')[1]
-    }else if(req.headers.cookie && req.headers.cookie.startsWith('sid')){
-        const allCookies = req.headers.cookie.split(';')
-        const tokenCookie = allCookies.filter(cookie => cookie.includes('sid'))
-        token = tokenCookie.toString().split('=')[1]
-        
-    }
-
-    if(!token) {
+    // if(!token || !token.includes('sid')) {
+    //     const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
+    //     return sendFailureResponse({
+    //         res, 
+    //         statusCode: nonAuthorizedError.statusCode, 
+    //         message: nonAuthorizedError.message
+    //     });
+    // }
+    if(!token && !token.includes('sid')) {
         const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
         return sendFailureResponse({
-            res,
-            statusCode:nonAuthorizedError.statusCode,
-            message:nonAuthorizedError.message
-        })
+            res, 
+            statusCode: nonAuthorizedError.statusCode, 
+            message: nonAuthorizedError.message
+        });
     }
 
-    verify(token,process.env.JWT_KEY!, (error:any,decodedToken:any) => {
+    const allCookies = token.toString().split(';');
+    const tokenCookie = allCookies.filter(cookie => cookie.includes('sid'));
+    token = tokenCookie[0].split('=')[1];
+
+    
+    verify(token, process.env.JWT_KEY!, (error:any, decodedToken:any)=> {
         if(error) {
-            const unauthorizedError = new NotAuthorizedError('There')
-            return sendFailureResponse({
-                res,
-                statusCode:unauthorizedError.statusCode,
-                message:unauthorizedError.message
-            })
+            const unauthorizedError = new NotAuthorizedError('There was an error')
+            return sendFailureResponse({ res, statusCode: unauthorizedError.statusCode, message: unauthorizedError.message });
         }
 
         getAuthUserByAuthAccessToken(decodedToken.id, token!)
-        .then((foundUser) => {
+        .then((foundUser)=> {
             if(!foundUser) {
                 const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
                 return sendFailureResponse({
-                    res,
-                    statusCode:nonAuthorizedError.statusCode,
-                    message:nonAuthorizedError.message
-                })
+                    res, 
+                    statusCode: nonAuthorizedError.statusCode, 
+                    message: nonAuthorizedError.message
+                });
+            }
+            
+            req.currentUser = {
+                id: foundUser.id,
+                staffObjectId: foundUser.staffObjectId,
+                staffId: foundUser.staffId,
+                email: foundUser.email,
+                firstname: foundUser.firstname
             }
 
-            req.currentUser={
-                id:foundUser.id,
-                staffObjectId:foundUser.staffObjectId,
-                staffId:foundUser.staffId,
-                email:foundUser.email,
-                firstname:foundUser.firstname
-            }
-
-            next()
+            next();
         })
-        .catch((error) => {
+        .catch((error)=> {
             console.log(error)
-            const nonAuthorizedError = new NotAuthorizedError('Unauthorized');
-                return sendFailureResponse({
-                res,
-                statusCode: nonAuthorizedError.statusCode,
+            const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
+            return sendFailureResponse({
+                res, 
+                statusCode: nonAuthorizedError.statusCode, 
                 message: nonAuthorizedError.message
             });
         })
     })
 }
+
+
+
+
 
 
 // import { Request, Response, NextFunction } from "express";
@@ -76,64 +79,64 @@ export default function validateToken(req:Request,res:Response,next:NextFunction
 // import { NotAuthorizedError } from "../server/Error";
 // import { getAuthUserByAuthAccessToken } from "../../services/db/auth.service";
 
-// export default function validateToken (req:Request, res:Response, next:NextFunction) {
-//     let token:string  = req.headers.authorization! ?? req.headers.cookie;
+// export default function validateToken(req:Request,res:Response,next:NextFunction){
+//     let token:string | undefined;
 
-//     // if(!token || !token.includes('sid')) {
-//     //     const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
-//     //     return sendFailureResponse({
-//     //         res, 
-//     //         statusCode: nonAuthorizedError.statusCode, 
-//     //         message: nonAuthorizedError.message
-//     //     });
-//     // }
-//     if(!token && !token.includes('sid')) {
-//         const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
-//         return sendFailureResponse({
-//             res, 
-//             statusCode: nonAuthorizedError.statusCode, 
-//             message: nonAuthorizedError.message
-//         });
+//     if(req.headers.authorization && req.headers.authorization.startsWith('bearer')){
+//         token = req.headers.authorization.split(' ')[1]
+//     }else if(req.headers.cookie && req.headers.cookie.startsWith('sid')){
+//         const allCookies = req.headers.cookie.split(';')
+//         const tokenCookie = allCookies.filter(cookie => cookie.includes('sid'))
+//         token = tokenCookie.toString().split('=')[1]
+        
 //     }
 
-//     const allCookies = token.toString().split(';');
-//     const tokenCookie = allCookies.filter(cookie => cookie.includes('sid'));
-//     token = tokenCookie[0].split('=')[1];
+//     if(!token) {
+//         const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
+//         return sendFailureResponse({
+//             res,
+//             statusCode:nonAuthorizedError.statusCode,
+//             message:nonAuthorizedError.message
+//         })
+//     }
 
-    
-//     verify(token, process.env.JWT_KEY!, (error:any, decodedToken:any)=> {
+//     verify(token,process.env.JWT_KEY!, (error:any,decodedToken:any) => {
 //         if(error) {
-//             const unauthorizedError = new NotAuthorizedError('There was an error')
-//             return sendFailureResponse({ res, statusCode: unauthorizedError.statusCode, message: unauthorizedError.message });
+//             const unauthorizedError = new NotAuthorizedError('There')
+//             return sendFailureResponse({
+//                 res,
+//                 statusCode:unauthorizedError.statusCode,
+//                 message:unauthorizedError.message
+//             })
 //         }
 
 //         getAuthUserByAuthAccessToken(decodedToken.id, token!)
-//         .then((foundUser)=> {
+//         .then((foundUser) => {
 //             if(!foundUser) {
 //                 const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
 //                 return sendFailureResponse({
-//                     res, 
-//                     statusCode: nonAuthorizedError.statusCode, 
-//                     message: nonAuthorizedError.message
-//                 });
-//             }
-            
-//             req.currentUser = {
-//                 id: foundUser.id,
-//                 staffObjectId: foundUser.staffObjectId,
-//                 staffId: foundUser.staffId,
-//                 email: foundUser.email,
-//                 firstname: foundUser.firstname
+//                     res,
+//                     statusCode:nonAuthorizedError.statusCode,
+//                     message:nonAuthorizedError.message
+//                 })
 //             }
 
-//             next();
+//             req.currentUser={
+//                 id:foundUser.id,
+//                 staffObjectId:foundUser.staffObjectId,
+//                 staffId:foundUser.staffId,
+//                 email:foundUser.email,
+//                 firstname:foundUser.firstname
+//             }
+
+//             next()
 //         })
-//         .catch((error)=> {
+//         .catch((error) => {
 //             console.log(error)
-//             const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
-//             return sendFailureResponse({
-//                 res, 
-//                 statusCode: nonAuthorizedError.statusCode, 
+//             const nonAuthorizedError = new NotAuthorizedError('Unauthorized');
+//                 return sendFailureResponse({
+//                 res,
+//                 statusCode: nonAuthorizedError.statusCode,
 //                 message: nonAuthorizedError.message
 //             });
 //         })

f93d65976b9bcfa3947fb9574615a708c472538c
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2024-02-21 11:17:42 +0100

    staff registration and staff edit

--- src/api/features/staff/controllers/roles/fetchStaffRoles.ts
+++ src/api/features/staff/controllers/roles/fetchStaffRoles.ts
@@ -2,22 +2,29 @@ import { Request, Response } from "express";
 import fetchAllStaffRoles from "../../services/roles/fetchAllStaffRoles";
 import { sendFailureResponse, sendSuccessResponse } from "@globals/server/serverResponse";
 import { ServerError } from "@globals/server/Error";
+import staffroleModel from "@staff/model/staffrole.model";
 
-export default function fetchStaffRoles(req:Request, res:Response) {
+export default async function fetchStaffRoles(req:Request, res:Response) {
     fetchAllStaffRoles()
-    .then((responseData)=> {
-        return sendSuccessResponse({ 
-            res, 
-            statusCode: 200, 
-            message: "Staff roles retrieved successfully", 
-            data: responseData 
-        })
-    })
-    .catch((error)=> {
+    // .then((responseData)=> {
+    //     return sendSuccessResponse({ 
+    //         res, 
+    //         statusCode: 200, 
+    //         message: "Staff roles retrieved successfully", 
+    //         data: responseData 
+    //     })
+    // })
+    // .catch((error)=> {
         
-        console.log("There was an error fetching all staff roles ", error);
+    //     console.log("There was an error fetching all staff roles ", error);
 
-        const serverError = new ServerError();
-        return sendFailureResponse({res, statusCode: serverError.statusCode, message: serverError.message})
-    })
+    //     const serverError = new ServerError();
+    //     return sendFailureResponse({res, statusCode: serverError.statusCode, message: serverError.message})
+    // })
+    try {
+        const fetchStaffRole = await staffroleModel.find().sort({ createdAt: -1 });
+        res.status(200).json(fetchStaffRole);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 }
